54d4c7ae697a09058e66b5f1a0d6befbf898969b
fw777888 <fw777888@gmail.com>
2025-10-07 02:35:00 +0300

    1.1 added BinarySearch & FileUtil

--- src/main/java/Search/BinarySearch.java
+++ src/main/java/Search/BinarySearch.java
@@ -1,7 +1,23 @@
 package Search;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class BinarySearch {
 
     /* TODO: Задание D
         Реализовать бинарный поиск с использованием универсальных дженериков под любые классы программы */
+    public static <T> int binarySearch(List<T> list, T key, Comparator<T> cmp) {
+        int l = 0, r = list.size() - 1;
+        while (l <= r) {
+            int m = (l + r) >>> 1;
+            int c = cmp.compare(list.get(m), key);
+            if (c == 0) return m;
+            if (c < 0) l = m + 1;
+            else r = m - 1;
+        }
+        return -1;
+    }
+
 }
+
--- src/main/java/Util/FileUtil.java
+++ src/main/java/Util/FileUtil.java
@@ -1,5 +1,11 @@
 package Util;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.List;
+
 public class FileUtil {
 
     /* TODO: Задание D
@@ -10,4 +16,15 @@ public class FileUtil {
         ДОПОЛНИТЕЛЬНОЕ ЗАДАНИЕ 2:
         Реализовать функционал для записи отсортированных коллекций/найденных значений в файл в режиме добавления данных.
      */
+    public static <T> void appendToFile(String file, List<T> items) {
+        try (FileWriter fw = new FileWriter(file, true);
+             BufferedWriter bw = new BufferedWriter(fw)) {
+            for (T t : items) {
+                bw.write(t.toString());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
 }
