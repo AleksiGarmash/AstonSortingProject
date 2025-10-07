a70139629f1f77455d9eb6ecc5c213fd596207fa
Aleksi <aleshka609@gmail.com>
2025-10-07 22:55:14 +0400

    Created menu, logic prescribed, created CustomList

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -1,5 +1,12 @@
 package App;
 
+import Collection.CustomList;
+import Model.CustomClass;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     /* TODO: Задание Е (меню, логика работы, интеграция всех частей)
@@ -9,5 +16,88 @@ public class Main {
      */
 
 
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        CustomList<CustomClass> list = new CustomList<>();
+        System.out.println("=== Приложение сортировки данных кастомных классов ===");
+
+        while (true) {
+            System.out.println(
+                    "\nМеню: \n" +
+                            "1. Заполнение данных вручную\n" +
+                            "2. Заполнение данных рандомно\n" +
+                            "3. Заполнение данных из файла\n" +
+                            "4. Сортировка ... \n" +
+                            "5. Сортировка ... \n" +
+                            "6. Бинарный поиск\n" +
+                            "== Дополнительное ==\n" +
+                            "7. Сортировка ... \n" + // чет\нечет
+                            "8. Запись данных в файл\n" +
+                            "9. Подсчет вхождений (многопоточно)\n" +
+                            "0. Выход\n" +
+                            "\nВыбор: "
+
+            );
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1" -> fillManual(list);
+                case "2" -> fillRandom(list);
+                case "3" -> fillFromFile(list);
+                case "4" -> runSort();
+                case "5" -> runSort();
+                case "6" -> runBinarySearch();
+                case "7" -> runEvenOddSort();
+                case "8" -> appendToFile();
+                case "9" -> countOccurrences();
+                case "0" -> {
+                    System.out.println("Выход...");
+                    return;
+                }
+                default -> System.out.println("Неверный ввод");
+            }
+        }
+    }
+
+    private static void fillManual(CustomList<CustomClass> list) {
+        list.clear();
+
+    }
+
+    private static void fillRandom(CustomList<CustomClass> list) {
+        list.clear();
+        Random random = new Random();
+
+    }
+
+    private static void fillFromFile(CustomList<CustomClass> list) {
+        list.clear();
+        System.out.println("Файл: ");
+        String path = scanner.nextLine();
+
+    }
+
+    private static <T> void runSort() {
+
+    }
+
+    private static void runBinarySearch() {
+
+    }
+
+    private static void runEvenOddSort() {
+
+    }
+
+    private static void appendToFile() {
+
+    }
+
+    private static void countOccurrences() {
+
+    }
 }
 
--- src/main/java/Collection/CustomList.java
+++ src/main/java/Collection/CustomList.java
@@ -1,10 +1,59 @@
 package Collection;
 
-public class CustomList {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CustomList<T> implements Iterable<T> {
 
     /* TODO:
         ДОПОЛНИТЕЛЬНОЕ ЗАДАНИЕ 3:
         Заполнение коллекций должно осуществляться посредством стримов.
         Коллекции для заполнения должны быть кастомными.
         */
+
+    private final List<T> data = new ArrayList<>();
+
+    public void add(T element) {
+        data.add(element);
+    }
+
+    public T get(int index) {
+        return data.get(index);
+    }
+
+    public void set(int index, T element) {
+        data.set(index, element);
+    }
+
+    public int size() {
+        return data.size();
+    }
+
+    public void clear() {
+        data.clear();
+    }
+
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    public Stream<T> stream() {
+        return data.stream();
+    }
+
+    public List<T> asList() {
+        return data;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
 }
