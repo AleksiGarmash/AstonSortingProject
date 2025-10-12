aa02ae8dabcc445ef3022b6457e1d182f1ec0996
Dmitry Vasilenko <Vasilenko_dima1811@mail.ru>
2025-10-12 13:19:13 +0300

    Extra1 - Добавил классы без реализации для сортировки чётных числовых значений

---
+++ src/main/java/Sorting/EvenFieldBubbleSort.java
@@ -0,0 +1,11 @@
+package Sorting;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class EvenFieldBubbleSort<T> implements SortStrategy<T>{
+    @Override
+    public void sort(List<T> list, Comparator<T> comparator) {
+
+    }
+}
